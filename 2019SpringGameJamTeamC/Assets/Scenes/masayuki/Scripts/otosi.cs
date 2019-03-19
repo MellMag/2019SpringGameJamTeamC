25e169088417316fd768c3e53bdbb0222ebcdedb
sutamina0 <gogomasa55yda@gmail.com>
2019-03-19 15:32:54 +0900

    [add]物の生成

---
+++ 2019SpringGameJamTeamC/Assets/Scenes/masayuki/Scripts/otosi.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class otosi : MonoBehaviour
+{
+    float time;
+    float speed;
+    public GameObject mono;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0;
+        speed = 3;
+        Destroy(gameObject, 2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Rigidbody2D>().velocity
+                            = -transform.up * speed;
+    }
+}
---
+++ 2019SpringGameJamTeamC/Assets/Scenes/masayuki/Scripts/seisei.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class seisei : MonoBehaviour
+{
+  
+    float time;
+    public GameObject Image;
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time >= 2)
+        {
+            Instantiate(Image, transform.position, transform.rotation);
+            time = 0f;
+        }
+    }
+}
