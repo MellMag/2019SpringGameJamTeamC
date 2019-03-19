d313a24d75208f12bcc8a85af3ca86f868f3f292
miyazaki44 <mhikaru0131@i.softbank.jp>
2019-03-19 16:59:40 +0900

    オブジェクトが一個ずつ消えます

---
+++ 2019SpringGameJamTeamC/Assets/Script/miyazaki/Object.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object : MonoBehaviour
+{
+    public int HP;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //HP = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void ClickeOnHit()
+    {
+        HP--;
+        if (HP == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
