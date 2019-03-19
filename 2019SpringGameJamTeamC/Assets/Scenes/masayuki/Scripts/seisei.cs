9aa35ff3de80512381b997b573c15faeb9d0ae67
sutamina0 <gogomasa55yda@gmail.com>
2019-03-19 15:56:38 +0900

    [add]横の座標をランダムで生成

--- 2019SpringGameJamTeamC/Assets/Scenes/masayuki/Scripts/seisei.cs
+++ 2019SpringGameJamTeamC/Assets/Scenes/masayuki/Scripts/seisei.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class seisei : MonoBehaviour
 {
-  
+    float x;
+    float y;
+    float z;
     float time;
     public GameObject Image;
     // Start is called before the first frame update
@@ -19,7 +21,11 @@ public class seisei : MonoBehaviour
         time += Time.deltaTime;
         if (time >= 2)
         {
-            Instantiate(Image, transform.position, transform.rotation);
+            x = Random.Range(-9.0f, -2.0f);
+            y = Random.Range(5.0f, 5.0f);
+            z = Random.Range(0.0f, 0.0f);
+
+            Instantiate(Image, new Vector3(x,y,z), transform.rotation);
             time = 0f;
         }
     }
