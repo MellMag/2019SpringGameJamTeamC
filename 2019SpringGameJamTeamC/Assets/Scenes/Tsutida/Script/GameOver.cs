3c5df6e0046cdb032f732dc746c9744d2bcc8433
DA\nwuser <tsubasa232451@gmail.com>
2019-03-20 13:31:13 +0900

    [add]ゲームオーバーの実装

---
+++ 2019SpringGameJamTeamC/Assets/Scenes/Tsutida/Script/GameOver.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    public bool YesActive;
+    public int cnt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        YesActive = false;
+        cnt = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (YesActive == true)
+        {
+            cnt += 1;
+        }
+        if (cnt >= 300)
+        {
+            SceneManager.LoadScene("Result");
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Finish")
+        {
+            YesActive = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Finish")
+        {
+            YesActive = false;
+            cnt = 0;
+        }
+    }
+
+}
